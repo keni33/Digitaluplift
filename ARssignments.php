abff2e99574a70fd70d361b4d88c7181b3e8186b
keni33 <udeokekeni@gmail.com>
2024-12-04 14:26:21 +0100

    Add files via upload

---
+++ ARssignments.php
@@ -0,0 +1,20 @@
+    <?php
+    //scalar types(single data type )
+    $name = "Ude Rita<br>";
+    echo $name;
+
+    $int = 17;
+    echo "$int<br>";
+    
+    $float = 7.6543;
+    echo "$float<br>";
+    
+    //array types(multiple data type) 
+     $fullName = array("Daniel", "Bella", "Frida", "Paul", "Alex");
+     echo "<pre>";
+     print_r($fullName);
+     echo "</pre><br>";
+     //var_dump($fullName);
+
+
+    ?>
